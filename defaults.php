d4245ba8d92eae9493302ac1fca1970f7f02523d
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2024-12-11 14:10:12 +0100

    Create defaults.php
    When config.php is not found, this file will be copied and used as a default. By default all mods except coreMods are disabled.

---
+++ defaults.php
@@ -0,0 +1,27 @@
+<?php
+    # User configuration file
+    ###########################################################
+
+    ### Enable or disable mods here ###
+
+    # FlatView removes borders around pages, which creates an overall flat design experience.
+    $FlatView = FALSE;
+
+    # HiddenLogin hides the login button, you can still access the login page with Alt+L.
+    $HiddenLogin = FALSE;
+
+    # TopNav adds the page navigation to the top of each page.
+    $TopNav = FALSE;
+
+    # This will make the search bar at the top square.
+    $SquareSearch = FALSE;
+
+    # This will remove various hover coloring on UI elements, the opacity will still change.
+    $NoBgColorOnHover = FALSE;
+
+    # Makes cards round.
+    $RoundCards = FALSE;
+
+    # Makes icons and text bigger and removes the background circle.
+    $BiggerIcons = FALSE;
+?>
