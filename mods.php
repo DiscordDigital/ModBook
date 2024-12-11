bbe66a756cfbfc6ed5824b01a8934b54535204ae
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2024-12-11 14:08:51 +0100

    Create mods.php
    This file contains lists of available mods.

---
+++ mods.php
@@ -0,0 +1,10 @@
+<?php
+    # Mods file
+    # Enable or disable mods in config.php
+    
+    # Core mods load by default and don't need to be enabled
+    $coreMods = ["WaitFor"];
+
+    # List of available mods, only load if enabled in config.php
+    $modList = ["FlatView", "HiddenLogin", "TopNav", "SquareSearch", "NoBgColorOnHover", "RoundCards", "BiggerIcons"];
+?>
