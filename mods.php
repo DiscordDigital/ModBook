31a7d8ab2290f129fdafadb0cf087a351f7a7e02
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2024-12-20 21:54:05 +0100

    Update mods.php
    Linked coreMod TinyMCEStyles, this will load global styles into the TinyMCE editor.

--- mods.php
+++ mods.php
@@ -3,7 +3,7 @@
     # Enable or disable mods in config.php
     
     # Core mods load by default and don't need to be enabled
-    $coreMods = ["WaitFor"];
+    $coreMods = ["WaitFor", "TinyMCEStyles"];
 
     # List of available mods, only load if enabled in config.php
     $modList = ["FlatView", "HiddenLogin", "TopNav", "SquareSearch", "NoBgColorOnHover", "RoundCards", "BiggerIcons", "DarkestMode", "SimplerCallouts"];
