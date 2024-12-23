4c9fdd9e414c3e5f8692477da37ecdbdf0f1765f
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2024-12-23 22:07:29 +0100

    Update mods.php
    Added BoldButtons to the modList

--- mods.php
+++ mods.php
@@ -6,5 +6,5 @@
     $coreMods = ["WaitFor", "TinyMCEStyles"];
 
     # List of available mods, only load if enabled in config.php
-    $modList = ["FlatView", "HiddenLogin", "TopNav", "SquareSearch", "NoBgColorOnHover", "RoundCards", "BiggerIcons", "DarkestMode", "SimplerCallouts", "ComplimentIcons"];
+    $modList = ["FlatView", "HiddenLogin", "TopNav", "SquareSearch", "NoBgColorOnHover", "RoundCards", "BiggerIcons", "DarkestMode", "SimplerCallouts", "ComplimentIcons", "BoldButtons"];
 ?>
