6985761f56fa9827fd979ca40d34952bdebe07d1
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2024-12-29 21:00:11 +0100

    Create entities.php
    Custom translations for entities.php

---
+++ lang/en/entities.php
@@ -0,0 +1,10 @@
+<?php
+    # Check if the function customTranslate is not defined
+    if (!function_exists("customTranslate")) {
+        # Include file that contains customTranslate
+        include "custom.php";
+    }
+
+    # Run customTranslate and return the results
+    return customTranslate("entities");
+?>
